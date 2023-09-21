d21d33efe0265b8cc2fd92ff1c4bdd90c4c64742
Fortune Nyasha Mhirimo <139254193+fortunenyasha@users.noreply.github.com>
2023-09-21 13:34:33 +0200

    Add files via upload

---
+++ tetris/tetris/Class6.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace tetris
+{
+    class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new Lblock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock()
+        };
+        private readonly Random random = new Random();
+        public Block NextBlock { get; private set; }
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBock();
+        }
+
+        private Block RandomBlock()
+        {
+            return blocks[Random.Next(blocks.length)];
+
+        }
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+            do
+            {
+                NextBlock = RandomBlock ();
+            }
+            while (block.Id == NextBlock.Id);
+            return block;
+        }
+
+    }
+}
---
+++ tetris/tetris/Class7.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tetris
+{
+    public class GameState 
+    {
+        private Block CurrentBlock;
+        public Block currentBlock
+        {
+            get => CurrentBlock;
+            private set
+            {
+                currentBlock = value;
+                currentBlock.Reset();
+            }
+        }
+        public GameGrid GameGrid { get; }
+        public BlockQueue BlockQueue { get; }
+        public  bool GameOver { get; private set; }
+        public GameState()
+        {
+            GameGrid = new GameGrid(22, 10);
+            BlockQueue = new BlockQueue();
+            CurrentBlock = BlockQueue.GetAndUpdate();
+        }
+        private bool Blockfits()
+        {
+            foreach (Position p in CurrentBlock.TilePositions())
+            {
+                if (!GameGrid.IsEmpty(p.Row, p.Column))
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+        //method to rotate block clockwise
+        private void rotateBlockCW () {
+            CurrentBlock.RotateCW();
+            if(!Blockfits())
+            {
+                CurrentBlock.RotateCW();
+            }
+        }
+        public void RotateBlockCCW()
+        {
+            currentBlock .RotateCCW();
+            if(!Blockfits()) {
+                currentBlock.RotateCCW();
+            }
+        }
+        public void MoveBlockLeft()
+        {
+            CurrentBlock.Move(0, -1);
+            if (!Blockfits())
+            {
+                CurrentBlock.Move(0, 1);
+            }
+        }
+        public void MoveBlockRight()
+        {
+            CurrentBlock.Move(0, 1);
+            if (!Blockfits())
+            {
+                CurrentBlock.Move(0, -1);
+            }
+        }
+        private bool IsGameOver()
+        {
+            return !(GameGrid.IsRowEmpty(0) && GameGrid.IsRowEmpty(1));
+        }
+        private void PlaceBlock()
+        {
+            foreach (Position p in CurrentBlock.TilePositions())
+            {
+                GameGrid[p.Row, p.Column] = CurrentBlock.Id;
+
+            }
+            GameGrid.ClearFullRows();
+            if (IsGameOver() )
+            {
+                GameOver = true;
+            }
+            else
+            {
+                CurrentBlock = BlockQueue.GetAndUpdate();
+            }
+        }
+        public void MoveBlockDown ()
+        {
+            CurrentBlock.Move(-1, 0);
+            PlaceBlock();
+        }
+    }
+
+}
